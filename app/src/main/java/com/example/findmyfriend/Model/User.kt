da4e93bd74287a75074e48c3a7bd72eb970904f1
user <aksiam468@gmail.com>
2024-10-22 13:57:24 +0600

    new push

--- app/src/main/java/com/example/findmyfriend/Model/User.kt
+++ app/src/main/java/com/example/findmyfriend/Model/User.kt
@@ -1,3 +1,22 @@
 package com.example.findmyfriend.Model
 
-data class User()
+import com.google.firebase.firestore.PropertyName
+
+
+data class User(
+    val userId: String,
+    @get:PropertyName("displayName")
+    @set:PropertyName("displayName")
+    var displayName: String = "",
+
+    @get:PropertyName("email")
+    @set:PropertyName("email")
+    var email: String = "",
+
+    @get:PropertyName("location")
+    @set:PropertyName("location")
+    var location: String = ""
+) {
+    // No-argument constructor
+    constructor() : this("", "", "")
+}
--- app/src/main/java/com/example/findmyfriend/View/MapsActivity.kt
+++ app/src/main/java/com/example/findmyfriend/View/MapsActivity.kt
@@ -2,6 +2,10 @@ package com.example.findmyfriend.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.findmyfriend.R
+import com.example.findmyfriend.Viewmodel.FirestoreViewModel
+import com.example.findmyfriend.databinding.ActivityMainBinding
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -14,35 +18,55 @@ import com.example.findmyfriend.databinding.ActivityMapsBinding
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-    private lateinit var binding: ActivityMapsBinding
+    private val binding by lazy {
+        ActivityMapsBinding.inflate(layoutInflater)
+    }
+    private lateinit var firestoreViewModel: FirestoreViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMapsBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        firestoreViewModel = ViewModelProvider(this).get(FirestoreViewModel::class.java)
+
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        binding.btnZoomIn.setOnClickListener {
+            mMap.animateCamera(CameraUpdateFactory.zoomIn())
+        }
+        binding.btnZoomOut.setOnClickListener {
+            mMap.animateCamera(CameraUpdateFactory.zoomOut())
+        }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        firestoreViewModel.getAllUsers(this) {
+            for (user in it) {
+                val userLocation = user.location
+                val latLng = parseLocation(userLocation)
+                if (userLocation.isEmpty()||userLocation == "Don't found any location yet"||userLocation == "Location not available") {
+                    LatLng(37.4220936, -122.083922)
+                }else{
+                    val markerOptions = MarkerOptions().position(latLng).title(user.displayName)
+                    googleMap.addMarker(markerOptions)
+                }
+                val cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 15f)
+                googleMap.animateCamera(cameraUpdate)
+            }
+        }
+    }
+
+    private fun parseLocation(location: String): LatLng {
+        val latLngSplit = location.split(", ")
+        val latitude = latLngSplit[0].substringAfter("Lat: ").toDouble()
+        val longitude = latLngSplit[1].substringAfter("Long: ").toDouble()
+        return LatLng(latitude, longitude)
     }
 }
